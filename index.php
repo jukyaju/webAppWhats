78ec63527c200129a5fee3ced54cd22e749b1a83
julian azure Ed2 <julian_az_linux@outlook.es>
2024-02-29 19:13:14 +0100

    Version2

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curso Azure / Linux</title>
+    <style>
+        body{
+            text-align:center
+        }
+    </style>
 </head>
 <body>
     <h1>Curso Azure / Linux</h1>
+    <p>
+        <img src="./images/QRWhatsAppAzure2.jpeg" alt="Imagen">
+    </p>
 </body>
 </html>
