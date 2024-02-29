061bb4c125f507c4f58eef3e38a388ee109763cb
julian azure Ed2 <julian_az_linux@outlook.es>
2024-02-29 19:02:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# webAppWhats" 
+Esto es un ejemplo para el curso de Azure / Linux
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Azure / Linux</title>
+</head>
+<body>
+    <h1>Curso Azure / Linux</h1>
+</body>
+</html>
